959c22bfe2b89ac18819ee718f3c08512667ce2e
Dvuglaf <aizeensan@gmail.com>
2022-05-04 17:23:15 +0400

    edit random generation and add parameters

--- lab4_correlator.py
+++ lab4_correlator.py
@@ -10,7 +10,7 @@ from scipy.signal import convolve2d
 def subplot(figure, n, m, pos, arr, title):
     sb = figure.add_subplot(n, m, pos)
     sb.set_title(title)
-    imshow(arr, cmap='gray', vmin=0, vmax=255)
+    imshow(arr, cmap='gray')
 
 
 # Наложение на фон(background) count-штук объектов(obj) с яркостью 128
@@ -27,12 +27,15 @@ def image_with_objects(background, count, obj):
                           [60, 3], [60, 13], [60, 23], [60, 33], [60, 43], [60, 53], [60, 60]], dtype=np.int32
                          )
 
-    for i in range(count):
-        idx = positions[np.random.randint(0, len(positions))]
+    i = 0
+    while i != count:
+        idx = positions[np.random.randint(0, len(positions) - 1)]
         if copy_background[idx[0]][idx[1]] == 128:  # Если на этом месте уже есть объект, то генерируем другой
             i -= 1
             continue
         copy_background[idx[0]][idx[1]] = 128
+        i += 1
+
     result = convolve2d(copy_background, obj, mode='same', boundary='symm', fillvalue=0)
     return result
 
@@ -50,9 +53,10 @@ def add_zeros(image):
 
 # Корреляционный метод распознавания объектов(obj) на изображении(image)
 def correlation_method(image, obj):
+    image_size = image.shape[0]
     x = copy.copy(image)
     x = add_zeros(x)
-    R = np.zeros((64, 64), dtype=np.float32)
+    R = np.zeros((image_size, image_size), dtype=np.float32)
 
     # Нахождение энергии t(obj)
     t_energy = 0
@@ -60,8 +64,8 @@ def correlation_method(image, obj):
         for l in range(0, obj.shape[1]):
             t_energy += (obj[k][l] ** 2)
 
-    for n in range(1, 65):
-        for m in range(1, 65):
+    for n in range(1, image_size + 1):
+        for m in range(1, image_size + 1):
             nominator = 0  # B = x(n + k, m + l)*t(k, l)        (k, l) from D
             denominator = 0  # x_energy = {x(n + k, m + l)}^2   (k, l) from D
 
@@ -76,18 +80,21 @@ def correlation_method(image, obj):
 
 
 def main():
+    OBJECTS_COUNT = 8
+    IMAGE_SIZE = 64
+    THRESHOLD = 0.93
     # Объекты
-    t = np.array([[1, 1, 1], [0, 1, 0], [0, 1, 0]], dtype=np.int32)
-    rev_t = np.array([[0, 1, 0], [0, 1, 0], [1, 1, 1]], dtype=np.int32)
+    T = np.array([[1, 1, 1], [0, 1, 0], [0, 1, 0]], dtype=np.int32)
+    REV_T = np.array([[0, 1, 0], [0, 1, 0], [1, 1, 1]], dtype=np.int32)
 
-    black_background = np.zeros((64, 64), dtype=np.int32)
+    black_background = np.zeros((IMAGE_SIZE, IMAGE_SIZE), dtype=np.int32)
 
     # Изображения с объектами на черном фоне
-    objects_t = image_with_objects(black_background, 4, t)  # буквы Т размером 3х3
-    objects_rev_t = image_with_objects(black_background, 4, rev_t)  # перевернутые буквы Т размером 3х3
+    objects_t = image_with_objects(black_background, OBJECTS_COUNT, T)  # буквы Т размером 3х3
+    objects_rev_t = image_with_objects(black_background, OBJECTS_COUNT, REV_T)  # перевернутые буквы Т размером 3х3
     objects_t_and_rev_t = objects_t + objects_rev_t  # буквы Т и перевернутые буквы Т размером 3х3
 
-    noise_background = np.random.normal(0, np.sqrt(np.var(objects_t)), (64, 64)).astype(
+    noise_background = np.random.normal(0, np.sqrt(np.var(objects_t)), (IMAGE_SIZE, IMAGE_SIZE)).astype(
         np.int32)  # фон - белый шум(0,sigma(obj))
 
     # Исходные изображения
@@ -97,32 +104,32 @@ def main():
     image_with_t_and_rev_t = objects_t_and_rev_t + noise_background  # Т и перевернутые Т на фоне
 
     # Корреляционные поля
-    R_image_none_1 = correlation_method(image_none, t)  # кор. поля для изображения без объектов
-    R_image_none_2 = correlation_method(image_none, rev_t)
+    R_image_none_1 = correlation_method(image_none, T)  # кор. поля для изображения без объектов
+    R_image_none_2 = correlation_method(image_none, REV_T)
 
-    R_image_with_t_1 = correlation_method(image_with_t, t)  # кор. поля для изображений с Т
-    R_image_with_t_2 = correlation_method(image_with_t, rev_t)
+    R_image_with_t_1 = correlation_method(image_with_t, T)  # кор. поля для изображений с Т
+    R_image_with_t_2 = correlation_method(image_with_t, REV_T)
 
-    R_image_with_rev_t_1 = correlation_method(image_with_rev_t, t)  # кор. поля для изображений с rev Т
-    R_image_with_rev_t_2 = correlation_method(image_with_rev_t, rev_t)
+    R_image_with_rev_t_1 = correlation_method(image_with_rev_t, T)  # кор. поля для изображений с rev Т
+    R_image_with_rev_t_2 = correlation_method(image_with_rev_t, REV_T)
 
-    R_image_with_t_and_rev_t_1 = correlation_method(image_with_t_and_rev_t, t)  # кор. поля для изображений с Т и rev T
-    R_image_with_t_and_rev_t_2 = correlation_method(image_with_t_and_rev_t, rev_t)
+    R_image_with_t_and_rev_t_1 = correlation_method(image_with_t_and_rev_t, T)  # кор. поля для изображений с Т и rev T
+    R_image_with_t_and_rev_t_2 = correlation_method(image_with_t_and_rev_t, REV_T)
 
     thresholding = numpy.vectorize(lambda elem, threshold: 255 if elem >= threshold else 0)  # пороговая обработка
 
     # Изображения корреляционных полей после пороговой обработки
-    g_none_1 = thresholding(R_image_none_1, 0.9)
-    g_none_2 = thresholding(R_image_none_2, 0.9)
+    g_none_1 = thresholding(R_image_none_1, THRESHOLD)
+    g_none_2 = thresholding(R_image_none_2, THRESHOLD)
 
-    g_with_t_1 = thresholding(R_image_with_t_1, 0.9)
-    g_with_t_2 = thresholding(R_image_with_t_2, 0.9)
+    g_with_t_1 = thresholding(R_image_with_t_1, THRESHOLD)
+    g_with_t_2 = thresholding(R_image_with_t_2, THRESHOLD)
 
-    g_with_rev_t_1 = thresholding(R_image_with_rev_t_1, 0.9)
-    g_with_rev_t_2 = thresholding(R_image_with_rev_t_2, 0.9)
+    g_with_rev_t_1 = thresholding(R_image_with_rev_t_1, THRESHOLD)
+    g_with_rev_t_2 = thresholding(R_image_with_rev_t_2, THRESHOLD)
 
-    g_with_t_and_rev_t_1 = thresholding(R_image_with_t_and_rev_t_1, 0.9)
-    g_with_t_and_rev_t_2 = thresholding(R_image_with_t_and_rev_t_2, 0.9)
+    g_with_t_and_rev_t_1 = thresholding(R_image_with_t_and_rev_t_1, THRESHOLD)
+    g_with_t_and_rev_t_2 = thresholding(R_image_with_t_and_rev_t_2, THRESHOLD)
 
     # Отображение
     fig_1 = plt.figure()
